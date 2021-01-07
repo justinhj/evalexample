3bf3c4dbd5251ebf14b0f723bfb86dba6b418276
Justin Heyes-Jones <justinhj@gmail.com>
2021-01-06 19:51:21 -0800

    tidy up

--- src/main/scala/Scala3Eval.scala
+++ src/main/scala/Scala3Eval.scala
@@ -3,6 +3,7 @@ object Scala3Eval extends App:
   enum Exp:
     case Val(value: Int) extends Exp
     case Add(left: Exp, right: Exp) extends Exp
+    case Mul(left: Exp, right: Exp) extends Exp
     case Var(identifier: String) extends Exp
   
   type Env = Map[String, Int]
@@ -11,20 +12,26 @@ object Scala3Eval extends App:
   
   type WithEnv = Env ?=> Int
   
-  def summonEnv : Env ?=> Env = summon[Env]
-  
   def eval(exp: Exp): WithEnv =
     exp match
       case Var(id) => handleVar(id)
       case Val(value) => value
       case Add(l,r) => handleAdd(l,r)
+      case Mul(l,r) => handleMul(l,r)
 
   def handleAdd(l: Exp, r: Exp): WithEnv = eval(l) + eval(r)
-  
-  def handleVar(s: String): WithEnv =
-    summonEnv.get(s).get
+  def handleMul(l: Exp, r: Exp): WithEnv = eval(l) * eval(r)
 
-  val exp1 : Exp = Add(Var("z"), Add(Val(10), Add(Var("xx"), Var("y"))))
+  def handleVar(s: String): WithEnv =
+    summon[Env].get(s).get
+
+  val exp1 : Exp = Add(
+      Var("z"), 
+      Add(
+        Val(10), 
+        Mul(
+          Var("x"), 
+          Var("y"))))
   
   // Provide an environment and eval the expression
   {
