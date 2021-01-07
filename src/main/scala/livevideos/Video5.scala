0f6e9486b057e71ba1ab064a6869c999f75068fa
Justin Heyes-Jones <justinhj@gmail.com>
2021-01-06 22:39:50 -0800

    video 5 final cut

--- src/main/scala/livevideos/Video5.scala
+++ src/main/scala/livevideos/Video5.scala
@@ -2,8 +2,45 @@ package livevideos
 
 object Video5 extends App {
 
- 
+ def f[A,B](a: A): B = ???
   
+  // Total - every a returns b
+  // Deterministic - every a returns the same every time
+  // No side effects
+
+  trait Functor[F[_]]:
+    extension [A, B](x: F[A])
+      def ffmap(f: A => B): F[B]
+  
+//  type StringEither[A] = Either[String, A]
+//  
+//  given Functor[StringEither] with
+//    extension [A, B](x: StringEither[A])
+//      def ffmap(f: A => B): StringEither[B] = {
+//        x match {
+//          case Right(a) => Right(f(a))
+//          case Left(err) => Left(err)
+//        }
+//      }
+  
+  given eitherFunctor[E]: Functor[[A] =>> Either[E, A]] with
+    extension [A, B](x: Either[E,A])
+        def ffmap(f: A => B): Either[E,B] = {
+          x match {
+            case Right(a) => Right(f(a))
+            case Left(err) => Left(err)
+          }
+        }
+  
+  
+  val e1: Either[String, Int] = Right(10)
+  val e2: Either[Int, Int] = Left(10)
+
+  val e3 = e1.ffmap(a => a + 1)
+  val e4 = e2.ffmap(a => a + 1)
+
+
   // Functors and error handling
-  println("Things!")
+  println(e3)
+  println(e4)
 }
