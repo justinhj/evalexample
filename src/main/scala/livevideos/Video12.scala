9e77a337aa85a5f6108627dd1597b4dde5ec3964
Justin Heyes-Jones <justinhj@gmail.com>
2021-04-21 13:10:16 -0700

    video 12 final

--- src/main/scala/livevideos/Video12.scala
+++ src/main/scala/livevideos/Video12.scala
@@ -5,18 +5,39 @@ object Video12 extends App:
   import org.justinhj.typeclasses.monad.{eitherMonad, _}
   import org.justinhj.typeclasses.numeric.{given, _}
 
+  case class ReaderT[F[_],R,A](run: R => F[A])
+
+  object ReaderT:
+    def lift[F[_],R,A](fa: F[A]): ReaderT[F,R,A] = ReaderT(_ => fa)
+
+  given readerTMonad[F[_]: Monad,R]: Monad[[A1] =>> ReaderT[F,R,A1]] with
+    def pure[A](a:A): ReaderT[F,R,A] = ReaderT(_ => Monad[F].pure(a))
+
+    extension [A,B](far: ReaderT[F,R,A])
+      def flatMap(f: A => ReaderT[F,R,B]): ReaderT[F,R,B] = {
+        ReaderT((r: R) =>
+          val fa: F[A] = far.run(r)
+          val fb: F[B] = fa.flatMap(b => f(b).run(r))
+          fb
+        )
+      }
+
+  import Exp._
+  import Video12._
+
   enum EvalError:
     case InvalidSymboName
     case SymbolNotFound
     case DivisionByZero
 
-  type EvalResult[A] = Either[EvalError, A]
+  type EvalResult[A] = ReaderT[[A1] =>> Either[EvalError, A1], Env[A],A]
 
   // Implement Numeric for EvalResult
   given evalResultNumeric[A: Numeric]: Numeric[EvalResult[A]] with {
 
     def isZero(a: EvalResult[A]): Boolean = {
-      a match {
+
+      a.run(Map.empty[String,A]) match {
         case Right(a) if summon[Numeric[A]].isZero(a) => true
         case _ => false
       }
@@ -28,7 +49,7 @@ object Video12 extends App:
 
     def div(a: EvalResult[A], b: EvalResult[A]): EvalResult[A] = {
       if isZero(b) then
-        Left(EvalError.DivisionByZero)
+        ReaderT.lift(Left(EvalError.DivisionByZero))
       else
         a.map2(b)(_ / _)
     }
@@ -39,9 +60,9 @@ object Video12 extends App:
     }
 
     def mul(a: EvalResult[A], b: EvalResult[A]): EvalResult[A] = {
-        a.map2(b)((a, b) => a * b)
-
+      a.map2(b)((a, b) => a * b)
     }
+
   }
 
   enum Exp[A]:
@@ -56,12 +77,14 @@ object Video12 extends App:
 
   import Exp._
 
-  type WithEnv[A] = Env[A] ?=> EvalResult[A]
+  type WithEnv[A] = ReaderT[[A1] =>> Either[EvalError,A1], Env[A], A]
+
+  def summonEnv[A] : Env[A] ?=> Env[A] = summon[Env[A]]
 
   def eval[A : Numeric](exp: Exp[A]): WithEnv[A] =
     exp match
       case Var(id) => handleVar(id)
-      case Val(value) => Right(value)
+      case Val(value) => ReaderT.lift(Right(value))
       case Add(l,r) => handleAdd(l,r)
       case Sub(l,r) => handleSub(l,r)
       case Div(l,r) => handleDiv(l,r)
@@ -73,10 +96,11 @@ object Video12 extends App:
   def handleDiv[A : Numeric](l: Exp[A] , r: Exp[A] ): WithEnv[A] = eval(l) / eval(r)
 
   def handleVar[A](s: String): WithEnv[A] =
-    summon[Env[A]].get(s) match {
-      case Some(value) => Right(value)
-      case None => Left(EvalError.SymbolNotFound)
-    }
+    ReaderT((env: Env[A]) =>
+      env.get(s) match {
+        case Some(value) => Right(value)
+        case None => Left(EvalError.SymbolNotFound)
+    })
 
   // A sample expression
   val exp1 : Exp[Int] = Add(
@@ -89,18 +113,18 @@ object Video12 extends App:
 
   // Provide an environment and eval the expression
   {
-    given envMap: Env[Int] = Map("x" -> 7, "y" -> 6, "z" -> 22)
+    val envMap: Env[Int] = Map("x" -> 7, "y" -> 6, "z" -> 22)
 
-    val eval1 = eval(exp1)
+    val eval1 = eval(exp1).run(envMap)
 
     println(s"Eval exp gives $eval1")
   }
 
   // And again with a missing symbol
   {
-    given envMap: Env[Int] = Map("x" -> 17, "y" -> 10, "a" -> 2)
+    val envMap: Env[Int] = Map("x" -> 17, "y" -> 10, "a" -> 2)
 
-    val eval1 = eval(exp1)
+    val eval1 = eval(exp1).run(envMap)
 
     println(s"Eval exp gives $eval1")
   }
